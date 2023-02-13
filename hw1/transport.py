8bbccb93d09e69b1a2e2bb011ba64c49979cab20
Anastasiya Kravchuk-Kirilyuk <nastena95@gmail.com>
2023-02-13 17:33:51 -0500

    transport comments

--- hw1/transport.py
+++ hw1/transport.py
@@ -18,15 +18,15 @@ HEADER_SIZE = 4 + 4 + 1
 
 STRING_ENCODING = "utf8"
 
-
+# Format header data as bytes according to HEADER_FORMAT
 def format_header(*, size: int, id: MsgId, more: bool):
     return struct.pack(HEADER_FORMAT, (size, id, more))
 
-
+# Calculate the next message ID
 def increment_msgid(id: MsgId) -> MsgId:
     return MsgId((id + 1) % MAX_ID)
 
-
+# This session class handles low-level byte transfer
 class Session(abc.AsyncIterator[bytes]):
     reader: asyncio.StreamReader
     writer: asyncio.StreamWriter
@@ -39,20 +39,28 @@ class Session(abc.AsyncIterator[bytes]):
     # atomic operations when used with primitive keys, so it'd be fine anyway.
     pending_msgs: DefaultDict[MsgId, bytes]
 
+    # Initialize session
     def __init__(self, reader, writer):
         self.currId = MsgId(0)
         self.reader = reader
         self.writer = writer
         self.pending_msgs = defaultdict(bytes)
 
+    # Get the next fresh message ID in this session
     def fresh_id(self) -> MsgId:
         prev = self.currId
         self.curr_id = increment_msgid(self.curr_id)
         return prev
 
+    # Send a single message (s) in bytes 
+    # by breaking down the message into packets
+    # and sending each packet in order
     async def send(self, s: bytes) -> None:
+        # generate a new ID for this message
         msg_id = self.fresh_id()
 
+        # for each chunk of the message,
+        # assemble a packet with the corresponding header and send it
         for i in range(0, len(s), MAX_MSG_SIZE):
             chunk = s[i : i + MAX_MSG_SIZE]
             more = bool(s[i + MAX_MSG_SIZE :])
@@ -61,21 +69,29 @@ class Session(abc.AsyncIterator[bytes]):
             self.writer.write(packet)
             await self.writer.drain()
 
+    # Receive a single message in bytes
     async def recv_single(self) -> bytes:
+        # loop to receive all incoming packets
         while True:
+            # for each packet, read and unpack header, then read chunk 
             header = await self.reader.read(HEADER_SIZE)
             size, id, more = struct.unpack(HEADER_FORMAT, header)
             chunk = await self.reader.read(size)
             id = MsgId(id)
+            # add this chunk to the corresponding pending message by id
             self.pending_msgs[id] += chunk
+            # if this is the last chunk, the message is fully received
             if not more:
                 payload = self.pending_msgs[id]
+                # delete from pending and return full message
                 del self.pending_msgs[id]
                 return payload
 
+    # Initialize iterator for session
     def __aiter__(self) -> abc.AsyncIterator[bytes]:
         return self
 
+    # Iterator function to receive the next message
     async def __anext__(self) -> bytes:
         try:
             return await self.recv_single()
