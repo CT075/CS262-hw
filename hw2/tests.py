44ba63ffdc7626761a1c4e4d033b33c24c11c059
Cameron Wong <cam@camdar.io>
2023-03-07 23:51:13 -0500

    add unit tests
    Signed-off-by: Cameron Wong <cam@camdar.io>

---
+++ hw2/tests.py
@@ -0,0 +1,31 @@
+# This code has no "unit" functionality of note, but whatever
+
+from unittest import TestCase, main
+
+from model import Clock
+
+
+class TestClock(TestCase):
+    def test_init(self):
+        clock = Clock()
+        self.assertEqual(clock.ctr, 0)
+
+    def test_increment(self):
+        clock = Clock()
+        clock.increment()
+        self.assertEqual(clock.ctr, 1)
+
+    def test_msgRecUpdate(self):
+        clock = Clock()
+        clock.msgRecUpdate(5)
+        self.assertEqual(clock.ctr, 6)
+
+        clock.msgRecUpdate(3)
+        self.assertEqual(clock.ctr, 6)
+
+        clock.msgRecUpdate(6)
+        self.assertEqual(clock.ctr, 7)
+
+
+if __name__ == "__main__":
+    main()
